97a45dd7b1863492ce27243d9c6e27437c8ebb5b
lucky-KRMU <2501730142@krmu.edu.in>
2026-09-05 12:30:54 +0530

    create and complete isEven.js

---
+++ smart-utility-toolkit/modules/isEven.js
@@ -0,0 +1,11 @@
+// This is the file to check whether any given number is even or not
+
+function checkEven(num) {
+    if (num%2 == 0) {
+        return true;
+    } else {
+        false;
+    }
+}
+
+module.exports = {checkEven}
