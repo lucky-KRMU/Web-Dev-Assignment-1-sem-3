4591704804a79df041cda95e5e900b704f2aaa81
lucky-KRMU <2501730142@krmu.edu.in>
2026-09-05 12:28:44 +0530

    complete calculator.js

--- smart-utility-toolkit/calculator.js
+++ smart-utility-toolkit/calculator.js
@@ -1,4 +1,24 @@
 // This is the file to create a simple CLI based Calculator using pure In-built methods
 
 let operation = process.argv[2];
-console.log(operation);
+let num1 = Number(process.argv[3]);
+let num2 = Number(process.argv[4]);
+
+let output = 'Invalid Operation'; // initial condition for the output
+
+// checking and applying different operations
+if (operation.toLowerCase() === 'add') {
+    output = num1 + num2;
+} else if (operation.toLowerCase() === 'sub') {
+    output = num1 - num2;
+} else if (operation.toLowerCase() === 'mul') {
+    output = num1 * num2;
+} else if (operation.toLowerCase() === 'div') {
+    if (num2 == 0) {
+        console.error("Zero Division Error!"); 
+        return;
+    }
+    output = num1 / num2;
+} 
+
+console.log(output);
